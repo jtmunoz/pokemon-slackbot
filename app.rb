46af13cc94f3305392c35a7286719662d3b38dd0
jtmunoz <jordantmunoz@gmail.com>
2019-06-25 21:41:38 -0700

    adds pokedex rb

--- app.rb
+++ app.rb
@@ -2,16 +2,19 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 require 'net/http'
+require 'poke-api-v2'
 
 get '/' do
-  redirect '/pokedex' 
+ 	redirect '/pokedex' 
 end
 
 get '/pokedex' do
-  erb :pokedex
+ 	erb :pokedex
 end
 
 
 post '/pokedex' do
-	
+	puts PokeApi.get(type:    params[:pokemon][:type])
+	puts PokeApi.get(pokemon: params[:pokemon][:name])
+	redirect '/'
 end

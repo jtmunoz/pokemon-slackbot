03877830a85aa29b31af00be7b58ced5cc8470b0
jtmunoz <jordantmunoz@gmail.com>
2019-07-25 22:41:33 -0700

    updates gemfile, require pokemon apil in app

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 require 'net/http'
-
+require 'poke-api-v2'
 
 get '/' do
  	redirect '/pokedex' 
@@ -14,6 +14,7 @@ end
 
 
 post '/pokedex' do
+	puts params	
 	puts PokeApi.get(type:    params[:pokemon][:type])
 	puts PokeApi.get(pokemon: params[:pokemon][:name])
 	redirect '/'
